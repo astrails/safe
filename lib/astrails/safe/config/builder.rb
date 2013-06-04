e9ad2ceb797e125883ad6611fd2268f3ec6cc0e9
Vitaly Kushner <vitaly@astrails.com>
2013-06-04 16:16:49 +0300

    refactor calue method definitions
    use helper class methodds to define

--- lib/astrails/safe/config/builder.rb
+++ lib/astrails/safe/config/builder.rb
@@ -8,60 +8,72 @@ module Astrails
           data.each { |k, v| self.send k, v }
         end
 
-        SIMPLE_VALUES = %w/verbose dry_run local_only path command options user
-        host port password key secret bucket api_key container socket
-        service_net repo_path/
-        MULTI_VALUES = %w/skip_tables exclude files/
-        HASH_VALUES = %w/mysqldump tar gpg keep pgdump tar svndump sftp ftp mongodump/
-        MIXED_VALUES = %w/s3 local cloudfiles /
-        COLLECTIONS = %w/database archive repo/
 
-        SIMPLE_VALUES.each do |m|
-          define_method(m) do |value|
-            ensure_uniq(m)
-            @node.set m, value
+        class << self
+          def simple_value(*names)
+            names.each do |m|
+              define_method(m) do |value|
+                ensure_uniq(m)
+                @node.set m, value
+              end
+            end
           end
-        end
 
-        MULTI_VALUES.each do |m|
-          define_method(m) do |value|
-            value = value.map(&:to_s) if value.is_a?(Array)
-            @node.set_multi m, value
+          def multi_value(*names)
+            names.each do |m|
+              define_method(m) do |value|
+                value = value.map(&:to_s) if value.is_a?(Array)
+                @node.set_multi m, value
+              end
+            end
           end
-        end
 
-        HASH_VALUES.each do |m|
-          define_method(m) do |data={}, &block|
-            ensure_uniq(m)
-            ensure_hash(m, data)
-            @node.set m, Node.new(@node, data || {}, &block)
+          def hash_value(*names)
+            names.each do |m|
+              define_method(m) do |data = {}, &block|
+                ensure_uniq(m)
+                ensure_hash(m, data)
+                @node.set m, Node.new(@node, data || {}, &block)
+              end
+            end
           end
-        end
 
-        MIXED_VALUES.each do |m|
-          define_method(m) do |data={}, &block|
-            ensure_uniq(m)
-            if data.is_a?(Hash) || block
-              ensure_hash(m, data) if block
-              @node.set m, Node.new(@node, data, &block)
-            else
-              @node.set m, data
+          def mixed_value(*names)
+            names.each do |m|
+              define_method(m) do |data={}, &block|
+                ensure_uniq(m)
+                if data.is_a?(Hash) || block
+                  ensure_hash(m, data) if block
+                  @node.set m, Node.new(@node, data, &block)
+                else
+                  @node.set m, data
+                end
+              end
+            end
+          end
+
+          def collection(*names)
+            names.each do |m|
+              define_method(m) do |id, data={}, &block|
+                raise "bad collection id: #{id.inspect}" unless id
+                ensure_hash(m, data)
+
+                name = "#{m}s"
+                collection = @node.get(name) || @node.set(name, Node.new(@node, {}))
+                collection.set id, Node.new(collection, data, &block)
+              end
             end
           end
         end
 
-        COLLECTIONS.each do |m|
-          define_method(m) do |id, data={}, &block|
-
-            raise "bad collection id: #{id.inspect}" unless id
-            ensure_hash(m, data)
-
-            name = "#{m}s"
-            collection = @node.get(name) || @node.set(name, Node.new(@node, {}))
-
-            collection.set id, Node.new(collection, data, &block)
-          end
-        end
+        simple_value :verbose, :dry_run, :local_only, :path, :command,
+          :options, :user, :host, :port, :password, :key, :secret, :bucket,
+          :api_key, :container, :socket, :service_net, :repo_path
+        multi_value :skip_tables, :exclude, :files
+        hash_value :mysqldump, :tar, :gpg, :keep, :pgdump, :tar, :svndump,
+          :sftp, :ftp, :mongodump
+        mixed_value :s3, :local, :cloudfiles
+        collection :database, :archive, :repo
 
         private
 
